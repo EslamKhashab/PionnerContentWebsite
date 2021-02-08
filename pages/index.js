1efe5040c65415c5641a4dd6d5088cacd4e80ed3
kekOo <kekOo@IDEA-PC>
2021-02-08 15:11:30 +0200

    Fix Google Tag Manger

--- pages/index.js
+++ pages/index.js
@@ -23,14 +23,23 @@ const Home = ({lang, flag}) => {
         <meta name="keywords" content="" />
         <link rel="icon" href="/logo.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script dangerouslySetInnerHTML={{
-    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-nTIFQK_iIocLhcz6Fv3GAtRHXRXTbQhSGXGaznfyNko');`,
-  }}>
-  </script>
+         {/* Google Tag Manager */}
+    <script
+        dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${nTIFQK_iIocLhcz6Fv3GAtRHXRXTbQhSGXGaznfyNko}');`,
+        }}
+    />
+    {/* End Google Tag Manager */}
+    {/* Google Tag Manager (noscript) */}
+    <noscript
+        dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${nTIFQK_iIocLhcz6Fv3GAtRHXRXTbQhSGXGaznfyNko}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+    />
   <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -63,14 +72,23 @@ const Home = ({lang, flag}) => {
                     <meta name="description" content="" />
                     <meta name="keywords" content="" />
          */}
-         <script dangerouslySetInnerHTML={{
-    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-nTIFQK_iIocLhcz6Fv3GAtRHXRXTbQhSGXGaznfyNko');`,
-  }}>
-  </script>
+         {/* Google Tag Manager */}
+    <script
+        dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${nTIFQK_iIocLhcz6Fv3GAtRHXRXTbQhSGXGaznfyNko}');`,
+        }}
+    />
+    {/* End Google Tag Manager */}
+    {/* Google Tag Manager (noscript) */}
+    <noscript
+        dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${nTIFQK_iIocLhcz6Fv3GAtRHXRXTbQhSGXGaznfyNko}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+    />
   <script
             dangerouslySetInnerHTML={{
               __html: `
